0f8060992270e83ee8013b811dd4bc817b5aaa95
DionysusBacchus <xkhrys00@stud.fit.vutbr.cz>
2023-05-22 00:12:56 +0200

    trivial: fix indentation

--- src/components/List.js
+++ src/components/List.js
@@ -4,62 +4,62 @@ import ListItem from './ListItem';
 const uuid = require("uuid");
 
 function getTasksFromStorage() {
-    const savedData = localStorage.getItem('myData');
-    if (savedData) {
-        return JSON.parse(savedData);
-    }
-    return [];
+  const savedData = localStorage.getItem('myData');
+  if (savedData) {
+    return JSON.parse(savedData);
+  }
+  return [];
 }
 
 export default function List() {
-    const [tasksArray, setTasksArray] = useState(getTasksFromStorage());
+  const [tasksArray, setTasksArray] = useState(getTasksFromStorage());
 
-    useEffect(() => {
-        const string = JSON.stringify(tasksArray);
+  useEffect(() => {
+    const string = JSON.stringify(tasksArray);
 
-        localStorage.setItem('myData', string);
-    }, [tasksArray]);
+    localStorage.setItem('myData', string);
+  }, [tasksArray]);
 
-    const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState('');
 
-    const handleInputChange = (event) => {
-        setInputValue(event.target.value);
-      };
-    
-      const handleAddItem = (event) => {
-        if (event.key === 'Enter') {
-          event.preventDefault();
-          if (inputValue.trim() !== '') {
-            setTasksArray([...tasksArray, {id: uuid.v4(), text: inputValue, done: false}]);
-            setInputValue('');
-          }
-        }
-      };
-    
-      const handleDeleteItem = (id) => {
-        setTasksArray(tasksArray.filter(item => item.id !== id));
-      };
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
-    const handleDoneItem = (id, isDone) => {
-        setTasksArray(tasksArray.map(item => {
-            if (item.id === id) {
-                item.done = isDone;
-            }
-            return item;
-        }))
-    };
+  const handleAddItem = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      if (inputValue.trim() !== '') {
+        setTasksArray([...tasksArray, { id: uuid.v4(), text: inputValue, done: false }]);
+        setInputValue('');
+      }
+    }
+  };
 
-    return (
-      <div>
+  const handleDeleteItem = (id) => {
+    setTasksArray(tasksArray.filter(item => item.id !== id));
+  };
+
+  const handleDoneItem = (id, isDone) => {
+    setTasksArray(tasksArray.map(item => {
+      if (item.id === id) {
+        item.done = isDone;
+      }
+      return item;
+    }))
+  };
+
+  return (
+    <div>
       {tasksArray.map((item) => (
-        <ListItem item={item} key={item.id} handles={{deleteItem: handleDeleteItem, doneItem: handleDoneItem}}/>
+        <ListItem item={item} key={item.id} handles={{ deleteItem: handleDeleteItem, doneItem: handleDoneItem }} />
       ))}
       <input
-            type="text"
-            value={inputValue}
-            onChange={handleInputChange}
-            onKeyPress={handleAddItem}
-          />
-      </div>
-    );
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        onKeyPress={handleAddItem}
+      />
+    </div>
+  );
 }
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,41 +1,40 @@
 import React, { useEffect, useState } from 'react';
 
 
-export default function ListItem({item, handles}) {
-    const [isDone, setIsDone] = useState(item.done);
+export default function ListItem({ item, handles }) {
+  const [isDone, setIsDone] = useState(item.done);
 
-    const handleDeleteItem = () => {
-      handles.deleteItem(item.id);
-    }
-
-    const handleCheckboxChange = () => {
-      setIsDone(!isDone);
-    };
-
-    const labelStyle = {
-      textDecoration: isDone ? 'line-through' : 'none'
-    };
-    
-    useEffect(() => {
-      handles.doneItem(item.id, isDone);
-    }, [isDone]);
-
-    return (
-      <div key={item.id}>
-        <input 
-          type="checkbox"
-          checked={isDone}
-          onChange={handleCheckboxChange} 
-          name={item.id}
-          key={item.id}
-        />
-        <label style={labelStyle} htmlFor={item.id}>{item.text}</label>
-        <span className="edit-icon">&#9998;</span>
-        <button 
-          className="delete-icon"
-          onClick={handleDeleteItem}
-          >&#10006;</button>
-      </div>
-    );
+  const handleDeleteItem = () => {
+    handles.deleteItem(item.id);
   }
-  
+
+  const handleCheckboxChange = () => {
+    setIsDone(!isDone);
+  };
+
+  const labelStyle = {
+    textDecoration: isDone ? 'line-through' : 'none'
+  };
+
+  useEffect(() => {
+    handles.doneItem(item.id, isDone);
+  }, [isDone]);
+
+  return (
+    <div key={item.id}>
+      <input
+        type="checkbox"
+        checked={isDone}
+        onChange={handleCheckboxChange}
+        name={item.id}
+        key={item.id}
+      />
+      <label style={labelStyle} htmlFor={item.id}>{item.text}</label>
+      <span className="edit-icon">&#9998;</span>
+      <button
+        className="delete-icon"
+        onClick={handleDeleteItem}
+      >&#10006;</button>
+    </div>
+  );
+}
