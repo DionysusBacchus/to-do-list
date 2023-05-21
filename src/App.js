f0a86535e825492067e6eac24725f0ee139d57d4
DionysusBacchus <xkhrys00@stud.fit.vutbr.cz>
2023-05-21 19:00:46 +0200

    clean App, move functionality to List

--- src/App.js
+++ src/App.js
@@ -3,43 +3,13 @@ import './App.css';
 import List from './components/List';
 import React, { createContext, useState } from 'react';
 
-
-let data = [
-  {text:"HHHH", done: true}, 
-  {text:"hohoho", done: false}, 
-  {text:"hihihi", done: false}, 
-  {text:"heheh", done: false}, 
-  {text:"huhuhu", done: false}
-];
 function App() {
-  const [inputValue, setInputValue] = useState('');
-  const [arrayValues, setArrayValues] = useState(data);
-
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleAddItem = (event) => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      if (inputValue.trim() !== '') {
-        setArrayValues([...arrayValues, {text: inputValue, done: false}]);
-        setInputValue('');
-      }
-    }
-  };
 
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <List data={arrayValues} />
-          <input
-            type="text"
-            value={inputValue}
-            onChange={handleInputChange}
-            onKeyPress={handleAddItem}
-          />
+          <List/>
         </div>
       </header>
     </div>
--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,46 @@
 import React, { useState } from 'react';
 import ListItem from './ListItem';
 
-export default function List({data}) {
+const uuid = require("uuid");
+
+export default function List() {
+
+    const [inputValue, setInputValue] = useState('');
+    const [arrayValues, setArrayValues] = useState(data);
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+      };
+    
+      const handleAddItem = (event) => {
+        if (event.key === 'Enter') {
+          event.preventDefault();
+          if (inputValue.trim() !== '') {
+            setArrayValues([...arrayValues, {id: uuid.v4(), text: inputValue, done: false}]);
+            setInputValue('');
+          }
+        }
+      };
 
     return (
       <div>
-      {data.map((item) => (
+      {arrayValues.map((item) => (
         <ListItem text={item.text} done={item.done} />
       ))}
+      <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            onKeyPress={handleAddItem}
+          />
       </div>
     );
   }
   
+  let data = [
+    {id: uuid.v4(), text:"HHHH", done: true}, 
+    {id: uuid.v4(), text:"hohoho", done: false}, 
+    {id: uuid.v4(), text:"hihihi", done: true}, 
+    {id: uuid.v4(), text:"heheh", done: false}, 
+    {id: uuid.v4(), text:"huhuhu", done: false}
+  ];
