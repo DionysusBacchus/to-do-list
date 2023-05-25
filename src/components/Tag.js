a1aefc60846f3aad4225d0d2b5751bf89f3b9deb
DionysusBacchus <xkhrys00@stud.fit.vutbr.cz>
2023-05-25 21:14:20 +0200

    implement tag text editing

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,7 +1,12 @@
 import React, { useContext } from 'react';
 import { FilterContext } from '../App';
+import InputField from './InputField';
 
 export default function Tag({ item, handles, noneTagId }) {
+
+  const [editing, setEditing] = React.useState(false);
+  const [newText, setNewText] = React.useState(item.text);
+
   const [chooseTag, buttonStyle, handleTagDelete] = useContext(FilterContext);
 
   const handleDeleteTag = () => {
@@ -9,26 +14,57 @@ export default function Tag({ item, handles, noneTagId }) {
     handles.deleteTag(item.id);
   }
 
+  const handleInputChange = (text) => {
+    setNewText(text);
+  }
+
+  const handleEditTag = () => {
+    if (newText.trim() === '') return;
+    handles.editTag(item.id, { ...item, text: newText });
+    setEditing(false);
+  }
+
+  if (item.id === noneTagId) return (
+    <button
+      onClick={() => chooseTag(item.id)}
+      style={buttonStyle(item.id)}>
+      {item.text}
+    </button>
+  );
+
+
   return (
     <div key={item.id}>
-      <button
-        onClick={() => chooseTag(item.id)}
-        style={buttonStyle(item.id)}>
-        {item.text}
-      </button>
-      {item.id !== noneTagId && (
+      {!editing ? (
         <span>
           <button
+            onClick={() => chooseTag(item.id)}
+            style={buttonStyle(item.id)}>
+            {item.text}
+          </button>
+
+          <button
+            onClick={() => setEditing(true)}
             className='edit-tag-button'>
             &#9998;
           </button>
-          <button
-            className='delete-tag-button'
-            onClick={handleDeleteTag}>
-            &#10006;
-          </button>
         </span>
-      )}
+      )
+        : (
+          <span>
+            <InputField initialValue={item.text} onChange={handleInputChange} onEnter={handleEditTag} />
+            <button
+              onClick={handleEditTag}
+              className='save-tag-button'>
+              &#x2713;
+            </button>
+          </span>
+        )}
+      <button
+        className='delete-tag-button'
+        onClick={handleDeleteTag}>
+        &#10006;
+      </button>
     </div>
   )
 }
