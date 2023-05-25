ed66a3f1f739ddefb5fa649c08c624e77b043d38
DionysusBacchus <xkhrys00@stud.fit.vutbr.cz>
2023-05-25 17:26:42 +0200

    move TagList into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import TaskList from './components/TaskList';
-import TagList from './components/TagList';
 import useFilter from './hooks/useFiler';
 import React, { createContext, useState } from 'react';
 import useList from './hooks/useList';
+import AddTag from './components/AddTag';
+import Tag from './components/Tag';
 
 const uuid = require("uuid");
 
@@ -28,11 +28,26 @@ function App() {
 
   }, []);
 
+  const addTag = (text) => {
+    const newTag = { id: uuid.v4(), text: text };
+    List.add(newTag);
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
         <FilterContext.Provider value={[chooseTag, buttonStyle]}>
-          <TagList tagsArray={tagsArray} List={List} />
+          <div>
+            <AddTag onAdd={addTag} />
+            {tagsArray.map((tag) => (
+              <span key={tag.id}>
+                <Tag
+                  item={tag}
+                  handles={{ deleteTag: List.delete, editTag: List.edit }} />
+              </span>
+            ))}
+          </div>
         </FilterContext.Provider>
         <TagContext.Provider value={{ tagsArray, noneTagId }}>
           <TaskList filerByTag={filerByTag} />
